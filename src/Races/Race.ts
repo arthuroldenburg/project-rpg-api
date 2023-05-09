8d4fc8916bbcc8006f4a4115fb118ce30c939681
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-05-09 10:54:34 -0300

    Feat: req 1 done

---
+++ src/Races/Race.ts
@@ -0,0 +1,16 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  constructor(readonly name: string, readonly dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  abstract maxLifePoints: number;
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
