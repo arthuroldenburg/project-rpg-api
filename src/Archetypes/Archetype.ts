b96db0557b15250e0b2216080ae612f83d92ce3d
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-05-09 14:02:13 -0300

    Feat: req 4 done

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,22 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  readonly name: string;
+  readonly special: number;
+  readonly cost: number;
+  static achetypeInstances = 0;
+  constructor(name: string) {
+    this.name = name;
+    this.special = 0;
+    this.cost = 0;
+    Archetype.achetypeInstances += 1;
+  }
+
+  static createdArchetypeInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract energyType: EnergyType;
+}
+
+export default Archetype;
